fc5f332ee9a3b7b50c24bf8cad0024f2948b50cf
mike-newh <zq@live.co.uk>
2019-01-08 16:42:31 +0000

    comment posting completed

--- src/components/ViewArticle/PostingComment.jsx
+++ src/components/ViewArticle/PostingComment.jsx
@@ -1,22 +1,43 @@
 import React, { Component } from 'react';
 import './PostingComment.css'
+import Axios from 'axios';
 
 class PostingComment extends Component {
+    state = {
+        body: '',
+        error: false,
+        posted: false,
+    }
     render() {
         return (
             <div id='PostingComment'>
                 <button onClick={this.props.closeNewComment} id='closeBox'>x</button>
-                <form id='commentForm'>
+                <form onSubmit={this.handleSubmit} id='commentForm'>
                     <label htmlFor='commentText'>Write a comment</label>
                     <div id='commentSubmitWrap'>
-                    <textarea cols='80' rows='4' type='text' id='commentText'></textarea> <button>Submit</button></div>
+                    <textarea onChange={this.handleChange} cols='80' rows='4' type='text' id='commentText'></textarea> <button>Submit</button></div>
                     <span>Posting as {this.props.user.username} on {this.props.article.title}</span>
+                    {this.state.error && <p>Error</p>}
+                    {this.state.posted && <p>Posted!</p>}
                 </form>
                 
             </div>
         );
     }
-    
+    handleSubmit = (e) => {
+        e.preventDefault()
+        if (!this.state.body){this.setState({error: true, posted: false})}
+        else {
+            const comment = {body: this.state.body, user_id: this.props.user.user_id}
+            Axios.post(`https://southcoders-news.herokuapp.com/api/articles/${this.props.article.article_id}/comments`, comment)
+            .then((res)=>{console.log('good post');
+            this.setState({error: false, posted: true})})
+            .catch(()=>{this.setState({error: true, posted: false})})
+        }
+    }
+    handleChange = (e) => {
+        this.setState({body: e.target.value})
+    }
 }
 
 export default PostingComment;
